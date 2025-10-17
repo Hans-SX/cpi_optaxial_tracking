a2739393502b592dd92caeab9dbbd09795a8678a
unknown <you@example.com>
2025-10-17 12:47:50 +0200

    irregular updat, seems changed a few without update to git.

--- moving_patterns.py
+++ moving_patterns.py
@@ -10,6 +10,7 @@ import numpy as np
 # from andor3 import Andor3
 from pipython import GCSDevice
 from pipython import pitools
+from itertools import cycle
 
 """
 Duplicated from the moving_patterns.py in acquisition_while_moving folder.
@@ -19,44 +20,69 @@ Ex. when the data set is acquired using BigStepForward_SmallStepBack(0, 17, 6, p
 
 class MotionPatterns(ABC):
     @abstractmethod
-    def generate(self): # -> List(float):
+    def _generate(self) -> np.array:
         pass
-    # def 
+    
+    @abstractmethod
+    def pos_frames(self) -> dict:
+        pass
 
 class BigStepForward_SmallStepBack(MotionPatterns):
-    def __init__(self, start:float, end:float, steps:int, interval:float=0.5, pattern = np.array((2, -1))):
+    def __init__(self, start:float, end:float, interval:float=0.5, pattern = np.array((2, -1))):
         self.start = start
         self.end = end
-        self.steps = steps
         self.interval = interval
-        self.pattern = pattern
+        self.pattern = cycle(pattern)
 
         if start < 0 or start > end or end > 17:
             raise ValueError("The range of M-231.17 actuator is 0 - 17 mm.")
-        elif start + (np.ceil(steps/2) * pattern[(steps - 1) % 2] + np.floor(steps/2) * pattern[steps % 2]) * interval > end or (np.ceil((steps-1)/2) * pattern[(steps - 2) % 2] + np.floor((steps-1)/2) * pattern[(steps-1) % 2]) * interval > end:
-            raise ValueError("The platform will go beyond the actuator range.")
             
+    def _generate(self):
+        current = self.start
+        pos = []
+        while current <= self.end:
+            pos.append(current)
+            current = current + next(self.pattern) * self.interval
+        
+        pos = np.asarray(pos)
+        if sum(pos < self.start) > 0 or sum(pos > self.end) > 0:
+            raise ValueError("The pattern exceed range 0 - 17 mm.")
+        return pos
     
-    def generate(self):
-        positions = [self.start + (np.ceil(x/2) * self.pattern[(x-1) % 2] + np.floor(x/2) * self.pattern[x % 2]) * self.interval for x in range(1, self.steps + 1)]
-        return positions
+    def pos_frames(self, interval=0.5, time_interval=100):
+        pfdict = dict()
+        pfdict['pos'] = self._generate()
+        total_length = sum(abs(pfdict['pos'][1:] - pfdict['pos'][:-1]))
+        time = int(total_length / interval)
+        pfdict['frames'] = time * time_interval
+        return pfdict
     
 class SinusoidalForward(MotionPatterns):
-    def __init__(self, start:float, end:float, steps:int, frequency:int=3, amp=3):
-        # At least 60 steps would looks like a sinusodial, 90 steps would be better. 
+    def __init__(self, start:float, end:float, npts:int, frequency:int=3, amp=3):
+        # At least 60 npts would looks like a sinusodial, 90 npts would be better. 
         self.start = start
         self.end = end
-        self.samples = np.linspace(start, end, steps)
+        self.samples = np.linspace(start, end, npts)
         self.freq = frequency
-        self.steps = steps
+        self.npts = npts
         self.amp = amp
 
         if start < 0 or start > end or end > 17:
             raise ValueError("The range of M-231.17 actuator is 0 - 17 mm.")
 
-    def generate(self):
-        positions = self.amp * np.sin(2 * np.pi * self.freq * self.samples / self.end) + self.samples
-        return positions
+    def _generate(self):
+        pos = self.amp * np.sin(2 * np.pi * self.freq * self.samples / self.end) + self.samples
+        if sum(pos < self.start) > 0 or sum(pos > self.end) > 0:
+            raise ValueError("The pattern exceed range 0 - 17 mm.")
+        return pos
+    
+    def pos_frames(self, interval=0.5, time_interval=100):
+        pfdict = dict()
+        pfdict['pos'] = self._generate()
+        total_length = sum(abs(pfdict['pos'][1:] - pfdict['pos'][:-1]))
+        time = int(total_length / interval)
+        pfdict['frames'] = time * time_interval
+        return pfdict
 
 def target_axials(x, n, stepsize):
     """
@@ -66,6 +92,10 @@ def target_axials(x, n, stepsize):
     left_values = [x - i * stepsize for i in range(1, n+1)]
     right_values = [x + i * stepsize for i in range(1, n+1)]
     return left_values[::-1] + [x] + right_values
+
+def shift(position, MA=4.2, MB=0.32, focal=30, pixA=5*4*6.5e-3, pixB=5*6.5e-3):
+    shift = - position * MA/MB / (position + focal) * pixB/pixA
+    return shift
 
 class Refocused_range():
     """
@@ -83,7 +113,7 @@ class Refocused_range():
         return try_shifts, expect_ref
 
     def fixed(self):
-        expect_ref = 6.87 - 0.25 * np.array(range(66))
+        expect_ref = 6.87 - 0.25 * np.array(range(69))
         try_ref_to = [target_axials(x, 20, 0.05) for x in expect_ref]
         try_shifts = [[self.shift(try_ref_to[x][y]) for y, _ in enumerate(try_ref_to[x])] for x, _ in enumerate(try_ref_to)]
         return try_shifts, expect_ref
@@ -107,6 +137,6 @@ class Refocused_range():
     def sinusoidal(self):
         expect_ref = 6.87 - self.pos
         # try_ref_to = [target_axials(x, 20, 0.05) for x in expect_ref]
-        try_ref_to = [target_axials(x, 5, 0.3) for x in expect_ref]
+        try_ref_to = [target_axials(x, 20, 0.05) for x in expect_ref]
         try_shifts = [[self.shift(try_ref_to[x][y]) for y, _ in enumerate(try_ref_to[x])] for x, _ in enumerate(try_ref_to)]
         return try_shifts, expect_ref
